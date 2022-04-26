362ef73ad465bc082e06ffa13273a785c58f64a8
William Ribeiro <93892383+devwilliamsr@users.noreply.github.com>
2022-04-26 11:34:47 -0300

    Adicionado o pacote Sentry em todos os projetos

--- src/Services/Api.AuthUsers/Controllers/AuthUsersController.cs
+++ src/Services/Api.AuthUsers/Controllers/AuthUsersController.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 namespace Api.AuthUsers.Controllers
 {
     public class AuthUsersController : ControllerBase
     {
-        
+        private readonly ILogger<AuthUsersController> _logger;  
+        public AuthUsersController(ILogger<AuthUsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+
     }
 }
